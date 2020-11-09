ed2e0f277923e9ad5bba6d83eaed693ad74883c7
Anthony Sanchez <asanchez2194@gmail.com>
2020-11-08 22:24:01 -0500

    adjusting css

--- my-app/src/components/JobBoardComponents.js
+++ my-app/src/components/JobBoardComponents.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const JobBoardComponent = ({job}) => (
-    <div className = 'flex'>
+    <div className = 'flex bg-white shadow-lg m-4 p-4'>
        <div>
            <img src={job.logo} alt={job.company}/>
        </div>
-       <div></div>
+       <div className = 'ml-3'>
+           <h3 className = 'font-bold text-teal-500'>{job.company}</h3>
+
+           <h2 className = 'font-bold text-xl'>{job.position}</h2>
+
+           <p>
+               {job.postedAt} · {job.contract} · {job.location}
+           </p>
+       </div>
        <div></div>
     </div>
 )
