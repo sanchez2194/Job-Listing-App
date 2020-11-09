201505f7efd65eddf019bb792b325f7b501ac8d0
Anthony Sanchez <asanchez2194@gmail.com>
2020-11-09 00:35:23 -0500

    finalized desktop version css, moving onto mobile version

--- my-app/src/components/JobBoardComponents.js
+++ my-app/src/components/JobBoardComponents.js
@@ -4,6 +4,7 @@ const JobBoardComponent = ({
     job: {
         company,
         logo,
+        isNew,
         featured,
         position,
         role,
@@ -13,35 +14,36 @@ const JobBoardComponent = ({
         location,
         languages,
         tools,
-        isNew
+
     },
 
 }) => {
 
     const tags = [role, level];
 
-    if(languages) {
+    if (languages) {
         tags.push(...languages);
     }
-   
-    if(tools) {
+
+    if (tools) {
         tags.push(...tools);
     }
 
-    
 
+    return (
+        <div className={`flex bg-white shadow-lg m-4 p-6 rounded
+        ${ featured && 'border-l-4 border-teal-500 border-solid'
+        }`} >
 
-
-    return(
-        <div className='flex bg-white shadow-lg m-4 p-6 rounded'>
             <div>
                 <img src={logo} alt={company} />
             </div>
+
             <div className='flex flex-col ml-4'>
                 <h3 className='font-bold text-teal-500'>
                     {company}
-                    {isNew && (<span>NEW</span>)}
-                    {isNew && (<span>NEW</span>)}
+                    {isNew && (<span className='font-bold bg-teal-500 text-teal-100 m-2 py-1 px-2 rounded-full'>New</span>)}
+                    {featured && (<span className='font-bold bg-gray-800 text-white py-1 px-2 rounded-full'>Featured</span>)}
                 </h3>
 
                 <h2 className='font-bold text-xl'>{position}</h2>
@@ -50,14 +52,17 @@ const JobBoardComponent = ({
                     {postedAt} · {contract} · {location}
                 </p>
             </div>
-            <div className = 'flex items-center ml-auto'>
+
+            <div className='flex items-center ml-auto'>
                 {tags ?
-                    tags.map((tag) => 
-                    <span className = 'flex font-bold bg-teal-100 text-teal-500 m-2 p-2 rounded'>{tag}</span>) : ''}
+                    tags.map((tag) =>
+                        <span className='flex font-bold bg-teal-100 text-teal-500 m-2 p-2 rounded'>{tag}</span>) : ''}
 
 
-       </div>
+            </div>
+
         </div>
-    )};
+    )
+};
 
 export default JobBoardComponent;
