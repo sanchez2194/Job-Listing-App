49dfc55d1b6af03300561b0f287d8e286c189b3f
Anthony Sanchez <asanchez2194@gmail.com>
2020-11-08 20:59:31 -0500

    setting up job board component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import JobBoardComponent from './components/JobBoardComponents';
+import JobBoardComponet from './components/JobBoardComponents';
 
 function App() {
   return (
     <div className="App">
       <h1>starting a new app</h1>
+      <JobBoardComponent />
     </div>
   );
 }
---
+++ my-app/src/components/JobBoardComponents.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JobBoardComponent = () => (
+    <div>
+        This is the Job Board!
+    </div>
+)
+
+export default JobBoardComponent;
