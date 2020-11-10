1bffd15e7dfa40f9a4fa9855732c008a72335304
Anthony Sanchez <asanchez2194@gmail.com>
2020-11-09 21:38:47 -0500

    needed to do mobile version first then made changes for desktop. problem solved with this push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import JobBoardComponent from './components/JobBoardComponents';
 import data from './assets/static-job-listings-master/data.json';
 
@@ -6,26 +6,26 @@ import data from './assets/static-job-listings-master/data.json';
 
 function App() {
   const [jobs, setJobs] = useState([]);
-  useEffect (() => {
-      setJobs(data);
-    }, []);
+  useEffect(() => {
+    setJobs(data);
+  }, []);
 
-    console.log(jobs);
+
 
   return (
     <div className="App">
-      <header className = 'bg-teal-400 mb-12'>
-        <img src= './images/bg-header-desktop.svg' alt= 'bg-pic' />
+      <header className='bg-teal-400 mb-12'>
+        <img src='./images/bg-header-desktop.svg' alt='bg-pic' />
       </header>
       {
-         jobs.length === 0 ? (
+        jobs.length === 0 ? (
           <p>fetching jobs...</p>
         ) : (
-          jobs.map( job =>
-            <JobBoardComponent job ={job} key = {job.id}/>
-          ))
+            jobs.map(job =>
+              <JobBoardComponent job={job} key={job.id} />
+            ))
       }
-      
+
     </div>
   );
 }
--- my-app/src/components/JobBoardComponents.js
+++ my-app/src/components/JobBoardComponents.js
@@ -31,32 +31,32 @@ const JobBoardComponent = ({
 
 
     return (
-        <div className={`flex bg-white shadow-lg m-4 p-6 rounded
-        ${ featured && 'border-l-4 border-teal-500 border-solid'
-        }`} >
+        <div className={`flex flex-col bg-white shadow-lg my-16 mx-10 p-6 rounded
+        ${featured && 'border-l-4 border-teal-500 border-solid'
+            } sm:flex-row`} >
 
             <div>
-                <img src={logo} alt={company} />
+                <img className='-mt-16 h-20 w-20 sm:my-0 sm:w-24 sm:h-24' src={logo} alt={company} />
             </div>
 
-            <div className='flex flex-col ml-4'>
+            <div className='flex flex-col justify-between ml-4'>
                 <h3 className='font-bold text-teal-500'>
                     {company}
-                    {isNew && (<span className='font-bold bg-teal-500 text-teal-100 m-2 py-1 px-2 rounded-full'>New</span>)}
-                    {featured && (<span className='font-bold bg-gray-800 text-white py-1 px-2 rounded-full'>Featured</span>)}
+                    {isNew && (<span className='font-bold bg-teal-500 text-teal-100 m-2 py-1 px-2 rounded-full uppercase text-sm'>New</span>)}
+                    {featured && (<span className='font-bold bg-gray-800 text-white py-1 px-2 rounded-full uppercase text-sm'>Featured</span>)}
                 </h3>
 
-                <h2 className='font-bold text-xl'>{position}</h2>
+                <h2 className='font-bold text-xl my-2'>{position}</h2>
 
                 <p>
                     {postedAt} · {contract} · {location}
                 </p>
             </div>
 
-            <div className='flex items-center ml-auto'>
+            <div className='flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0'>
                 {tags ?
                     tags.map((tag) =>
-                        <span className='flex font-bold bg-teal-100 text-teal-500 m-2 p-2 rounded'>{tag}</span>) : ''}
+                        <span className='flex font-bold bg-teal-100 text-teal-500 m-2 p-2 mr-4 mb-4 rounded sm:mb-0'>{tag}</span>) : ''}
 
 
             </div>
